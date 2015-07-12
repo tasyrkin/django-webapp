4532a3dd97c5f5c7a6c5f4fe0b8efd9ea72979a2
Timofey Asyrkin <tasyrkin@gmail.com>
2015-07-12 10:28:11 -0700

    Urls routing for polls webapp

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+def detail(request, question_id):
+    return HttpResponse("You are looking at the question %s." % question_id)
+
+def results(request, question_id):
+    return HttpResponse("You are looking at the results of question %s." % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You are voting on question %s." % question_id)
