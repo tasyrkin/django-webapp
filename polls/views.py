d781f490c27da65fdf5603e5fef43a99da206714
Timofey Asyrkin <tasyrkin@gmail.com>
2015-07-14 20:12:24 -0700

    Implemented a controller and view template for votes

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 from models import Question
 
@@ -15,4 +16,17 @@ def results(request, question_id):
     return HttpResponse("You are looking at the results of question %s." % question_id)
 
 def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except:
+        return render(request, 'polls/details.html',
+                      {
+                          'question': question,
+                          'error_message': 'No choice was selected.'
+                      })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args = (question.id,)))
     return HttpResponse("You are voting on question %s." % question_id)
